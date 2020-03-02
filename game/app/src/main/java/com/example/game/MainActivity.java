61ca966d6173e6ec2dea30b887754ae46c0277dd
sajakbasnet <versacenevein@gmail.com>
2020-03-02 20:40:13 +0545

    initial commit

---
+++ game/app/src/main/java/com/example/game/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int teamAScore;
+    private int teamBScore;
+    private TextView teamATextViewScore;
+    private TextView teamBTextViewScore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        teamATextViewScore = findViewById(R.id.team_a_score);
+        teamBTextViewScore = findViewById(R.id.team_b_score);
+    }
+
+    public void updateForTeamAPoint6(View view) {
+        teamAScore = teamAScore + 6;
+        displayScoreTeamA(teamAScore);
+    }
+
+    public void updateForTeamAPoint3(View view) {
+        teamAScore = teamAScore + 4;
+        displayScoreTeamA(teamAScore);
+    }
+
+    public void updateForTeamAPoint2(View view) {
+        teamAScore = teamAScore + 2;
+        displayScoreTeamA(teamAScore);
+    }
+
+    public void updateForTeamAPoint1(View view) {
+        teamAScore = teamAScore + 1;
+        displayScoreTeamA(teamAScore);
+    }
+
+    public void updateForTeamBPoint1(View view) {
+        teamBScore = teamBScore + 1;
+        displayScoreTeamB(teamBScore);
+    }
+
+    public void updateForTeamBPoint6(View view) {
+        teamBScore = teamBScore + 6;
+        displayScoreTeamB(teamBScore);
+    }
+
+    public void updateForTeamBPoint2(View view) {
+        teamBScore = teamBScore + 2;
+        displayScoreTeamB(teamBScore);
+    }
+
+    public void updateForTeamBPoint3(View view) {
+        teamBScore = teamBScore + 4;
+        displayScoreTeamB(teamBScore);
+    }
+
+    private void displayScoreTeamA(int score) {
+        teamATextViewScore.setText(String.valueOf(score));
+    }
+
+    private void displayScoreTeamB(int score) {
+        teamBTextViewScore.setText(String.valueOf(score));
+    }
+    public void reset(View v){
+        teamAScore=0;
+        teamBScore=0;
+        displayScoreTeamB(0);
+        displayScoreTeamA(0);
+
+    }
+}
